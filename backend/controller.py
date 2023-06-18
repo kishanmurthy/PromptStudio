f720d9a89927c8a9ae44820885e0b49ad17e53a9
kishanmurthy <kishan.murthy.n@gmail.com>
2023-06-18 00:01:14 -0700

    Shreya squashed commit

--- backend/controller.py
+++ backend/controller.py
@@ -2,10 +2,11 @@ import asyncio
 import random
 import uuid
 from urllib.parse import urlencode, urlparse
-from mongo_db import save_or_update
+from mongo_db import save_or_update, save_publish_version, find_published_dag_arch
 from dag_parser import create_dag_object
 from prompt_processing import prompt_process
-from aioflask import Flask, abort, jsonify, render_template, request
+from generate_code import generate_python_code
+from aioflask import Flask, abort, jsonify, render_template, request, send_file
 from flask_cors import CORS
 import json
 
@@ -32,13 +33,28 @@ def test_dag():
     return json.dumps(input_dicts)
 
 @app.route('/download', methods=["POST"])
-def test_dag_ye():
+def download_code():
     dag_arch = request.get_json()
     save_or_update(dag_arch)
     node_dict, tp_sort = create_dag_object(dag_arch)
     node_input = dag_arch["input"]
+    file_path = generate_python_code(dag_arch["flow_name"], dag_arch["version"], node_input, node_dict, tp_sort)
+    return send_file(file_path)
+
+@app.route('/published_endpoint/<flow_name>')
+def published_endpoints(flow_name):
+    dag_arch = find_published_dag_arch(flow_name)
+    node_dict, tp_sort = create_dag_object(dag_arch)
+    node_input = dag_arch["input"]
     output_dict, input_dicts = prompt_process(node_input, node_dict, tp_sort)
     return json.dumps(input_dicts)
+
+@app.route('/publish', methods=["POST"])
+def publish_code():
+    dag_arch = request.get_json()
+    save_or_update(dag_arch)
+    save_publish_version(dag_arch)
+    return jsonify({'statusCode':200})
 
 if __name__ == '__main__':
    app.run()
--- backend/mongo_db.py
+++ backend/mongo_db.py
@@ -5,7 +5,17 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 
 mydb = myclient["prompt_builder"]
 mycollection = mydb["prompt_archs"]
+published_collection = mydb["published_flow_versions"]
+
 mycollection.create_index(
+	[
+		("flow_name", 1),
+		("version", -1)
+	],
+	unique = True
+)
+
+published_collection.create_index(
 	[
 		("flow_name", 1),
 		("version", -1)
@@ -19,3 +29,23 @@ def save_or_update(json_data):
 		mycollection.insert_one(json_data)
 	else:
 		mycollection.replace_one({'flow_name': json_data["flow_name"], 'version': json_data["version"]}, json_data, True)
+
+
+def save_publish_version(json_data):
+	data_to_insert = {}
+	data_to_insert["flow_name"] = json_data["flow_name"]
+	data_to_insert["version"] = json_data["version"]
+	
+	data = published_collection.find_one({'flow_name': json_data["flow_name"], 'version': json_data["version"]})
+	if data is None:
+		published_collection.insert_one(data_to_insert)
+	else:
+		myquery = {'flow_name': json_data["flow_name"], 'version': json_data["version"]}
+		published_collection.delete_one(myquery)
+		published_collection.insert_one(data_to_insert)
+
+def find_published_dag_arch(flow_name):
+	data = published_collection.find_one({'flow_name': flow_name})
+	print(data)
+	dag_arch = mycollection.find_one({'flow_name': data["flow_name"], 'version': data["version"]})
+	return dag_arch
